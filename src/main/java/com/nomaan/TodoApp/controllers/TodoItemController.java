d7483619bfd4003aa0bcb9a86c7ea369a2a62a4d
nomaan-ahmad <nomaan.ahmad21@gmail.com>
2023-04-27 02:35:15 +0530

    Created intial controller and template to test

--- src/main/java/com/nomaan/TodoApp/controllers/TodoItemController.java
+++ src/main/java/com/nomaan/TodoApp/controllers/TodoItemController.java
@@ -1,2 +1,19 @@
-package com.nomaan.TodoApp.controllers;public class TodoItemController {
+package com.nomaan.TodoApp.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TodoItemController {
+    private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
+
+    @GetMapping(value = "/")
+    public ModelAndView index () {
+        logger.debug("request to GET index");
+        ModelAndView modelAndView = new ModelAndView("index");
+        return modelAndView;
+    }
 }
