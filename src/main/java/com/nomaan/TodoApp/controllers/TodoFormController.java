e262be54c2f230d1ecec1020563f08fa2f53a9d8
nomaan-ahmad <nomaan.ahmad21@gmail.com>
2023-04-27 15:04:40 +0530

    Added todo item editing functionality

--- src/main/java/com/nomaan/TodoApp/controllers/TodoFormController.java
+++ src/main/java/com/nomaan/TodoApp/controllers/TodoFormController.java
@@ -1,2 +1,46 @@
-package com.nomaan.TodoApp.controllers;public class TodoFormController {
+package com.nomaan.TodoApp.controllers;
+
+import com.nomaan.TodoApp.models.TodoItem;
+import com.nomaan.TodoApp.repositories.TodoItemRepository;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.time.Instant;
+
+@Controller
+public class TodoFormController {
+    private final Logger logger = LoggerFactory.getLogger(TodoFormController.class);
+    private final TodoItemRepository todoItemRepository;
+
+    public TodoFormController (TodoItemRepository todoItemRepository) {
+        this.todoItemRepository = todoItemRepository;
+    }
+
+    @GetMapping(value = "/edit/{id}")
+    public String showUpdateForm (@PathVariable("id") long id, Model model) {
+        TodoItem todoItem = todoItemRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("TodoItem id: " + id + " not found"));
+
+        model.addAttribute("todo", todoItem);
+        return "update-todo-item.html";
+    }
+
+    @PostMapping(value = "/todo/{id}")
+    public String updateTodoItem(@PathVariable long id, @Valid TodoItem todoItem, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            todoItem.setId(id);
+            return "update-todo-item.html";
+        }
+
+        todoItem.setModifiedDate(Instant.now());
+        todoItemRepository.save(todoItem);
+        return "redirect:/";
+    }
 }
