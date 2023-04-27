b662626351bcc00683f84b27a51bd6faf05acc84
nomaan-ahmad <nomaan.ahmad21@gmail.com>
2023-04-27 12:48:13 +0530

    Updated TodoItemController to fetch and return data from db

--- src/main/java/com/nomaan/TodoApp/controllers/TodoItemController.java
+++ src/main/java/com/nomaan/TodoApp/controllers/TodoItemController.java
@@ -1,5 +1,6 @@
 package com.nomaan.TodoApp.controllers;
 
+import com.nomaan.TodoApp.repositories.TodoItemRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -10,10 +11,16 @@ import org.springframework.web.servlet.ModelAndView;
 public class TodoItemController {
     private final Logger logger = LoggerFactory.getLogger(TodoItemController.class);
 
+    private final TodoItemRepository todoItemRepository;
+    public TodoItemController (TodoItemRepository todoItemRepository) {
+        this.todoItemRepository = todoItemRepository;
+    }
+
     @GetMapping(value = "/")
     public ModelAndView index () {
         logger.debug("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemRepository.findAll());
         return modelAndView;
     }
 }
